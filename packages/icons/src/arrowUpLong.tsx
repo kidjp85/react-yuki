f68b65fa29fb7cf1d61a0db105c42237c2d76936
Asher Nguyen <phucnguyenhoang1985@gmail.com>
2019-08-28 08:53:26 +1000

    removed unused icons

--- packages/icons/src/arrowUpLong.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import withIcon from './utils/withIcons';
-
-export default withIcon('ArrowUpLong', ({ color, strokeWidth }) => (
-  <g fill={color} stroke={color} strokeLinecap="square" strokeWidth={strokeWidth}>
-    <line fill="none" strokeLinecap="butt" x1="24" x2="24" y1="46" y2="3" />
-    <polyline fill="none" points="38,17 24,3 10,17 " stroke={color} />
-  </g>
-));
--- packages/icons/src/filter.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import withIcon from './utils/withIcons';
-
-export default withIcon('Filter', ({ color, strokeWidth }) => (
-  <g fill={color} stroke={color} strokeLinecap="square" strokeWidth={strokeWidth}>
-    <polygon fill="none" points="4 4 44 4 28 28 28 45 20 45 20 28 4 4" stroke={color} />
-  </g>
-));
