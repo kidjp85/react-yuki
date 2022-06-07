e5421c378e73828289d4a550239fea5b2847eda5
Asher Nguyen <anguyen@seek.com.au>
2022-06-07 17:07:43 +1000

    updated package

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,3 +1,5 @@
 export { default as useHover } from './useHover';
+
 export { default as usePrevious } from './usePrevious';
+
 export { default as useScrollToBottom } from './useScrollToBottom';
--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -1,58 +1,110 @@
 export { default as AddToCart } from './AddToCart';
+
 export { default as Alarm } from './Alarm';
+
 export { default as ArrowDown } from './ArrowDown';
+
 export { default as ArrowDownLong } from './ArrowDownLong';
+
 export { default as ArrowLeft } from './ArrowLeft';
+
 export { default as ArrowLeftLong } from './ArrowLeftLong';
+
 export { default as ArrowRightLong } from './ArrowRightLong';
+
 export { default as ArrowRight } from './ArrowRight';
+
 export { default as ArrowUp } from './ArrowUp';
+
 export { default as ArrowUpLong } from './ArrowUpLong';
+
 export { default as Attach } from './Attach';
+
 export { default as Cart } from './Cart';
+
 export { default as Chat } from './Chat';
+
 export { default as Check } from './Check';
+
 export { default as CloudDownload } from './CloudDownload';
+
 export { default as CloudUpload } from './CloudUpload';
+
 export { default as Cross } from './Cross';
+
 export { default as Currency } from './Currency';
+
 export { default as Download } from './Download';
+
 export { default as Edit } from './Edit';
+
 export { default as File } from './File';
+
 export { default as Filter } from './Filter';
+
 export { default as Heart } from './Heart';
+
 export { default as Info } from './Info';
+
 export { default as Key } from './Key';
+
 export { default as Mail } from './Mail';
+
 export { default as Menu } from './Menu';
+
 export { default as Phone } from './Phone';
+
 export { default as Question } from './Question';
+
 export { default as RemoveFromCart } from './RemoveFromCart';
+
 export { default as Search } from './Search';
+
 export { default as Setting } from './Setting';
+
 export { default as Tag } from './Tag';
+
 export { default as Trash } from './Trash';
+
 export { default as Upload } from './Upload';
+
 export { default as Warning } from './Warning';
+
 export { default as Calendar } from './Calendar';
+
 export { default as User } from './User';
+
 export { default as Spaceship } from './Spaceship';
+
 export { default as Code } from './Code';
+
 export { default as Copy } from './Copy';
+
 export { default as Compass } from './Compass';
+
 export { default as RobotVacuum } from './RobotVacuum';
 
 // Social icons
 export { default as Codepen } from './Codepen';
+
 export { default as Discord } from './Discord';
+
 export { default as Facebook } from './Facebook';
+
 export { default as Github } from './Github';
+
 export { default as Google } from './Google';
+
 export { default as Instagram } from './Instagram';
+
 export { default as Linkedin } from './Linkedin';
+
 export { default as Reddit } from './Reddit';
+
 export { default as Slack } from './Slack';
+
 export { default as Twitter } from './Twitter';
+
 export { default as Youtube } from './Youtube';
 
 // Types
